26be85ccdfc0e64608ff459d39432f746008266a
haroldks <kiossouharold@gmail.com>
2022-09-07 10:45:26 +0200

    [fundamentals] Add Union and Median implementation + tests.

---
+++ src/main/java/fundamentals/Median.java
@@ -0,0 +1,113 @@
+package fundamentals;
+
+
+/**
+ * Author Pierre Schaus
+ *
+ * We give you the API of a Vector class allowing to access,
+ * modify and exchange two elements in constant time.
+ * Your task is to implement a method to calculate the median of a Vector.
+ *
+ * public interface Vector {
+ *     // size of the vector
+ *     public int size();
+ *     // set the value v to the index i of the vector
+ *     public void set(int i, int v);
+ *     // returns the value at index i of the vector
+ *     public int get(int i);
+ *     // exchanges the values at positions i and j
+ *     public void swap(int i, int j);
+ * }
+ * You must implement a method that has the following signature:
+ * public static int median(Vector a, int lo, int hi)
+ *
+ * This method calculates the median of vector "a" between the positions "lo" and "hi" (included).
+ * You can consider that the vector "a" has always an odd size.
+ * To help you, an IntelliJ project with a minimalist test to check if your code computes the right median value is given.
+ * Indeed, it is not advised to debug your code via Inginious.
+ * Warning It is not forbidden to modify or swap elements of the vector "a" during the calculation (with the get/set/swap methods).
+ * It is forbidden to call other methods of the standard Java library. It is also forbidden to make a "new".
+ *
+ * The evaluation is based on 10 points:
+ *  - good return value: 3 points,
+ *  - good return value and complexity O(n log n): 5 points,
+ *  - good return value and complexity O(n) expected (average case on a random uniform distribution): 10 points.
+ *
+ *  All the code you write in the text field will be substituted in the place indicated below.
+ *  You are free to implement other methods to help you in this class, but the method "median" given above must at least be included.
+ */
+public class Median {
+
+    public static class Vector {
+
+        private int [] array;
+        private int nOp = 0;
+
+
+        public Vector(int n) {
+            array = new int[n];
+        }
+
+        public int size() {
+            return array.length;
+        }
+
+        public void set(int i, int v) {
+            nOp++;
+            array[i] = v;
+        }
+
+        public int get(int i) {
+            nOp++;
+            return array[i];
+        }
+
+        public void swap(int i, int j) {
+            nOp++;
+            int tmp = array[i];
+            array[i] = array[j];
+            array[j] = tmp;
+        }
+
+        public int nOp() {
+            return nOp;
+        }
+
+
+
+    }
+
+
+
+    // BEGIN STRIP
+    public static int partition(Vector a, int lo, int hi) {
+        int i = lo, j = hi+1;
+        int v = a.get(lo);
+        while (true) {
+            while (a.get(++i) < v) if (i == hi) break;
+            while (v < a.get(--j)) if (j == lo) break;
+            if (i >= j) break;
+            a.swap(i,j);
+        }
+        a.swap(lo,j);
+        return j;
+    }
+    // END STRIP
+
+    public static int median(Vector a, int lo, int hi) {
+        // TODO
+        // STUDENT return -1;
+        // BEGIN STRIP
+        int i = partition(a,lo,hi);
+        if (i == a.size()/2) return a.get(i);
+        else if (i < a.size()/2) {
+            return median(a,i+1,hi);
+        } else {
+            return median(a,lo,i-1);
+        }
+        // END STRIP
+    }
+
+
+
+}
---
+++ src/main/java/fundamentals/Union.java
@@ -0,0 +1,105 @@
+package fundamentals;
+
+// BEGIN STRIP
+import java.util.ArrayList;
+import java.util.Arrays;
+// END STRIP
+
+/**
+ * Author Pierre Schaus
+ *
+ * Given an array of (closed) intervals, you are asked to implement the union operation.
+ * This operation will return the minimal array of sorted intervals covering exactly the union
+ * of the points covered by the input intervals.
+ * For example, the union of intervals [7,9],[5,8],[2,4] is [2,4],[5,9].
+ * The Interval class allowing to store the intervals is provided
+ * to you and looks like this (you can use it directly in your code).
+ *
+ * public class Interval implements Comparable<Interval> {
+ *
+ *     final int min, max;
+ *
+ *     public Interval(int min, int max) {
+ *         assert(min <= max);
+ *         this.min = min;
+ *         this.max = max;
+ *     }
+ *
+ *     @Override
+ *     public boolean equals(Object obj) {
+ *         return ((Interval) obj).min == min && ((Interval) obj).max == max;
+ *     }
+ *
+ *     @Override
+ *     public String toString() {
+ *         return "["+min+","+max+"]";
+ *     }
+ *
+ *     @Override
+ *     public int compareTo(Interval o) {
+ *         if (min < o.min) return -1;
+ *         else if (min == o.min) return max - o.max;
+ *         else return 1;
+ *     }
+ * }
+ *
+ */
+public class Union {
+
+
+    public static class Interval implements Comparable<Union.Interval> {
+
+        public final int min;
+        public final int max;
+
+        public Interval(int min, int max) {
+            assert(min <= max);
+            this.min = min;
+            this.max = max;
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            return ((Union.Interval) obj).min == min && ((Union.Interval) obj).max == max;
+        }
+
+        @Override
+        public String toString() {
+            return "["+min+","+max+"]";
+        }
+
+        @Override
+        public int compareTo(Union.Interval o) {
+            if (min < o.min) return -1;
+            else if (min == o.min) return max - o.max;
+            else return 1;
+        }
+    }
+
+
+
+    public static Interval[] union(Interval[] intervals) {
+        // TODO
+        // STUDENT return new Interval[]{};
+        // BEGIN STRIP
+        if (intervals.length == 0) return intervals;
+        Arrays.sort(intervals);
+        int min = intervals[0].min;
+        int max = intervals[0].max;
+        ArrayList<Interval> res = new ArrayList<>();
+        for (int i = 1; i < intervals.length; i++) {
+            if (intervals[i].min > max) {
+                res.add(new Interval(min, max));
+                min = intervals[i].min;
+                max = intervals[i].max;
+            } else {
+                max = Math.max(max, intervals[i].max);
+            }
+        }
+        res.add(new Interval(min, max));
+        return res.toArray(new Interval[0]);
+        // END STRIP
+
+    }
+
+}
