a64e6c71b30e8085e84d060c2a11446b7a014430
haroldks <kiossouharold@gmail.com>
2022-09-21 09:41:39 +0200

    [graphs] ConnectedComponentTest parameterized.

--- src/test/java/graphs/ConnectedComponentsTest.java
+++ src/test/java/graphs/ConnectedComponentsTest.java
@@ -1,141 +1,134 @@
 package graphs;
 
-import com.github.guillaumederval.javagrading.Grade;
 import org.junit.Test;
+import org.junit.experimental.runners.Enclosed;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 import utils.Graph;
 
-import java.util.Random;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.*;
 
 import static org.junit.Assert.assertEquals;
 
-
+@RunWith(Enclosed.class)
 public class ConnectedComponentsTest {
 
-    @Test
-    public void exampleTest() {
-        int n = 9;
-        Graph g = new Graph(n);
-        g.addEdge(0, 1);
-        g.addEdge(1, 2);
-        g.addEdge(2, 5);
-        g.addEdge(3, 4);
-        g.addEdge(4, 7);
-        g.addEdge(6, 8);
-        assertEquals(3, ConnectedComponents.numberOfConnectedComponents(g));
+    @RunWith(Parameterized.class)
+    public static class TestCorrectness {
+
+        final Instance instance;
+
+        @Parameterized.Parameters(name = "{0}")
+        public static Collection data() {
+            LinkedList<Object[]> coll = new LinkedList<>();
+            for (int i = 0; i < 7; i++) {
+                String name = "data/graphs.ConnectedComponents/in_corr_" + i;
+                coll.add(new Object[]{name, new Instance(name)});
+            }
+            return coll;
+        }
+
+        public TestCorrectness(String name, Instance instance) {
+            this.instance = instance;
+        }
+
+        @Test
+        public void test() throws Exception {
+            int student_solution = ConnectedComponents.numberOfConnectedComponents(instance.graph);
+            assertEquals(student_solution, instance.solutions);
+        }
+
+
     }
 
-    // BEGIN STRIP
-    private static class WeightedQuickUnionUF {
-        private int[] parent;   // parent[i] = parent of i
-        private int[] size;     // size[i] = number of sites in subtree rooted at i
-        private int count;      // number of components
+    @RunWith(Parameterized.class)
+    public static class TestCycle {
 
-        public WeightedQuickUnionUF(int n) {
-            count = n;
-            parent = new int[n];
-            size = new int[n];
-            for (int i = 0; i < n; i++) {
-                parent[i] = i;
-                size[i] = 1;
+        final Instance instance;
+
+        @Parameterized.Parameters(name = "{0}")
+        public static Collection data() {
+            LinkedList<Object[]> coll = new LinkedList<>();
+            for (int i = 0; i < 2; i++) {
+                String name = "data/graphs.ConnectedComponents/in_cycl_" + i;
+                coll.add(new Object[]{name, new Instance(name)});
+            }
+            return coll;
+        }
+
+        public TestCycle(String name, Instance instance) {
+            this.instance = instance;
+        }
+
+        @Test
+        public void test() throws Exception {
+            int student_solution = ConnectedComponents.numberOfConnectedComponents(instance.graph);
+            assertEquals(student_solution, instance.solutions);
+        }
+
+
+    }
+
+
+    @RunWith(Parameterized.class)
+    public static class TestComplexity {
+
+        final Instance instance;
+
+        @Parameterized.Parameters(name = "{0}")
+        public static Collection data() {
+            LinkedList<Object[]> coll = new LinkedList<>();
+            for (int i = 0; i < 2; i++) {
+                String name = "data/graphs.ConnectedComponents/in_compl_" + i;
+                coll.add(new Object[]{name, new Instance(name)});
+            }
+            return coll;
+        }
+
+        public TestComplexity(String name, Instance instance) {
+            this.instance = instance;
+        }
+
+        @Test(timeout = 3000)
+        public void test() throws Exception {
+            int student_solution = ConnectedComponents.numberOfConnectedComponents(instance.graph);
+            assertEquals(student_solution, instance.solutions);
+        }
+
+
+    }
+
+
+    static class Instance {
+
+        Graph graph;
+
+        int solutions;
+
+        public Instance(String file) {
+
+            try {
+                Scanner dis = new Scanner(new FileInputStream(file));
+                String line = dis.nextLine();
+                String[] base_info = line.split(" ");
+                graph = new Graph(Integer.parseInt(base_info[0]));
+                int E = Integer.parseInt(base_info[1]);
+                for (int i = 0; i < 2 * E; i++) {
+                    line = dis.nextLine();
+                    String[] edge = line.split(" ");
+                    graph.addEdge(Integer.parseInt(edge[0]), Integer.parseInt(edge[1]));
+                }
+                solutions = dis.nextInt();
+
+
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
             }
         }
-
-
-        public int count() {
-            return count;
-        }
-
-        public int find(int p) {
-            validate(p);
-            while (p != parent[p])
-                p = parent[p];
-            return p;
-        }
-
-        // validate that p is a valid index
-        private void validate(int p) {
-            int n = parent.length;
-            if (p < 0 || p >= n) {
-                throw new IndexOutOfBoundsException("index " + p + " is not between 0 and " + (n - 1));
-            }
-        }
-
-        public boolean connected(int p, int q) {
-            return find(p) == find(q);
-        }
-
-        public void union(int p, int q) {
-            int rootP = find(p);
-            int rootQ = find(q);
-            if (rootP == rootQ) return;
-
-            // make smaller root point to larger one
-            if (size[rootP] < size[rootQ]) {
-                parent[rootP] = rootQ;
-                size[rootQ] += size[rootP];
-            } else {
-                parent[rootQ] = rootP;
-                size[rootP] += size[rootQ];
-            }
-            count--;
-        }
     }
-
-    public void testRandomGraphOk(int n, int e) {
-        Graph g = new Graph(n);
-        WeightedQuickUnionUF uf = new WeightedQuickUnionUF(n);
-        Random r = new Random(6);
-        for (int i = 0; i < e; i++) {
-            int orig = r.nextInt(n);
-            int dest = r.nextInt(n);
-            g.addEdge(orig, dest);
-            uf.union(orig, dest);
-        }
-        int nbCC = ConnectedComponents.numberOfConnectedComponents(g);
-        assertEquals(uf.count(), nbCC);
-    }
-
-    @Test
-    @Grade(value = 25)
-    public void cycleGraphOk() {
-        int n = 1002;
-        Graph g = new Graph(n);
-        WeightedQuickUnionUF uf = new WeightedQuickUnionUF(n);
-        g.addEdge(0, n - 1);
-        uf.union(0, n - 1);
-
-        for (int i = 1; i < n; i++) {
-            g.addEdge(i, (i - 1) % n);
-            uf.union(i, (i - 1) % n);
-        }
-
-        assertEquals(uf.count(), ConnectedComponents.numberOfConnectedComponents(g));
-    }
-
-
-    @Test(timeout = 3000)
-    @Grade(value = 25)
-    public void complexityOk() {
-        int n = 7002;
-        Graph g = new Graph(n);
-        for (int i = 0; i < n; i++) {
-            g.addEdge(i, (i + 2) % n);
-        }
-        ConnectedComponents.numberOfConnectedComponents(g);
-    }
-
-    @Test
-    @Grade(value = 50)
-    public void correctness() {
-        testRandomGraphOk(600, 120);
-        testRandomGraphOk(220, 7);
-        testRandomGraphOk(105, 3);
-        testRandomGraphOk(0, 0);
-        testRandomGraphOk(10, 2 * 10);
-        testRandomGraphOk(42, 23);
-        testRandomGraphOk(420, 123);
-        testRandomGraphOk(90, 40);
-    }
-
-    // END STRIP
 }
