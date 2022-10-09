b49ddc0e56bf64d178489a91fd402643033a6f1e
BurlatsAuguste <52482818+BurlatsAuguste@users.noreply.github.com>
2022-10-09 14:30:56 +0200

    Change the solution in Trains.java, add an example in the description of the exercise

--- src/main/java/graphs/Trains.java
+++ src/main/java/graphs/Trains.java
@@ -1,70 +1,57 @@
 package graphs;
 
+// BEGIN STRIP
 import java.util.*;
-import java.util.stream.Collector;
-import java.util.stream.Collectors;
+// END STRIP
 
+/**
+* Considering a list containing the relations between train stations (a train leaves the station `from` at `startTime` 
+* and arrives at station `to` at `endTime`) and the positions of those stations, a starting station and a starting time,
+* what is the earliest hour at which you can reach each an accessible station ?
+* 
+* You don't have to consider several points :
+* - passengers can leave a station at the exact moment where they reach this station
+* - all liaisons are direct
+* - timetable are not periodic, you don't have to repeat them everyday
+* - startTime < endTime and from != to are always true in all relations
+* - there is no duplicates entry (i.e. strictly equal relations)
+*
+* As an example, let's consider that your list of relations between train stations is :
+* {(Bxl-midi, 9:00 am) : [(Namur, 9:20 am), (Charleroi, 9:30 am), (Ottiginies, 9:20 am)],
+*  (Ottignies, 9:30 am) : [(LLN, 9:40 am), (Charleroi, 9:50) am],
+*  (Charleroi, 9:30 am) : [(Namur, 9:45 am)],
+*  (Charleroi, 9:50 am) : [(Ottignies, 10:00 am)],
+*  (Namur, 10:00 am) : [(Charleroi, 10:30 am)],
+*  (Ottignies, 10:00 am) : [(LLN, 10:20 am)]
+* }
+* 
+* In the above dictionary, the keys are the departure stations and times, and the values are a list of stations that you can reach 
+* (if you take a train starting from the key) and the time at which you would reach them.
+*
+*  The list of reacheables stations and the earliest hour you can reach them is :
+* {Bxl-midi : 9:00 am,
+*  Namur : 9:20 am,
+*  Charleroi, 9:30 am,
+*  Ottignies : 9:20 am,
+*  LLN : 9:40 am
+* }
+* 
+* We leave notion of optimal/reasonable complexity unclear on purpose. It is your job, based on your knowledge,
+* to identify, among the appropriate algorithm family, which one is optimal.
+* 
+* A clue : as you probably guessed it, it is clearly a graph problem. But it isn't a usual graph :
+* nodes are particular, because they doesn't represent only a point in the space, but also a point in the time
+* (for example (Bruxelles-midi, 8:48 am)).
+*
+* Don't forget that if I reach Bxl-midi at time i, I can take any train that leaves Bxl-midi at time j >= i.
+*
+* By the way, do you know the function TreeMap.subMap (https://docs.oracle.com/javase/8/docs/api/java/util/TreeMap.html#subMap-K-boolean-K-boolean-) ?
+*
+*/
 public class Trains {
 
-    public static class StationTime implements Comparable<StationTime> {
-
-        public final String pos; //the station
-        public final int time;  //the time
-    
-        public StationTime(String pos, int time) {
-            this.pos = pos;
-            this.time = time;
-        }
-    
-        @Override
-        public int hashCode() {
-            return pos.hashCode() ^ Integer.hashCode(~time);
-        }
-    
-        @Override
-        public boolean equals(Object obj) {
-            if(obj instanceof StationTime)
-                return ((StationTime) obj).pos.equals(pos) && ((StationTime) obj).time == time;
-            return false;
-        }
-    
-        @Override
-        public int compareTo(StationTime o) {
-            int out = time - o.time;
-            if(out == 0)
-                return pos.compareTo(o.pos);
-            return out;
-        }
-    }
-
-    /**
-     * Considering a list containing the relations between train stations (a train leaves the station `from` at `startTime` 
-     * and arrives at station `to` at `endTime`) and the positions of those stations, a starting station and a starting time,
-     * what is the earliest hour at which you can reach each an accessible station ?
-     * 
-     *You don't have to consider several points :
-     * - passengers can leave a station at the exact moment where they reach this station
-     * - all liaisons are direct
-     * - timetable are not periodic, you don't have to repeat them everyday
-     * - starTime < endTime and from != to are always true in all relations
-     * - there is no duplicates entry (i.e. strictly equal relations)
-     *
-     * The question is graduated on 20 :
-     * - 13 points if the algorithm works
-     * - 4 points if the algorithm is fast (reasonable complexity)
-     * - 3 points if the algorithm is really fast (optimal complexity)
-     * 
-     * We leave notion of optimal/reasonable complexity unclear on purpose. It is your job, based on your knowledge,
-     * to identify, among the appropriate algorithm family, which one is optimal.
-     * 
-     * A clue : as you probably guessed it, it is clearly a graph problem. But it isn't a usual graph :
-     * nodes are particular, because they doesn't represent only a point in the space, but also a point in the time
-     * (for example (Bruxelles-midi, 8:48 am)).
-     *
-     * Don't forget that if I reach Bxl-midi at time i, I can take any train that leaves Bxl-midi at time j >= i.
-     *
-     * By the way, do you know the function TreeMap.subMap (https://docs.oracle.com/javase/8/docs/api/java/util/TreeMap.html#subMap-K-boolean-K-boolean-) ?
-     *
+    /** 
+     * Considering given starting station and time, compute the earliest hour at which any accessible station can be reached.
      * @param relations a list of relations that connect a pair (station, time) (the key) (for exemple, Bxl-midi, 8:48 am)
      *                  with a list of trains that leave the station at this time, represented by a list of
      *                  StationTime objects that gives at each station/time those trains arrives.
@@ -75,37 +62,62 @@ public class Trains {
      *         The dictionnary must contain the starting station
      */
     public static Map<String, Integer> reachableEarliest(HashMap<StationTime, LinkedList<StationTime>> relations, StationTime startPoint) {
-        //STUDENT return null; 
-        //BEGIN STRIP
+        // STUDENT return null; 
+        // BEGIN STRIP
         HashMap<String, Integer> solution = new HashMap<>();
-        PriorityQueue<String> Q = new PriorityQueue<>((x,y) -> solution.get(x).compareTo(solution.get(y)));
-        HashMap<String, Boolean> visited = new HashMap<>();
+        PriorityQueue<StationTime> Q = new PriorityQueue<>((x,y) -> x.compareTo(y));
 
-        Q.add(startPoint.pos);
-        visited.put(startPoint.pos, false);
-        solution.put(startPoint.pos, startPoint.time);
+        for(StationTime key: relations.keySet()) 
+            Q.add(key);
+        
+        solution.put(startPoint.station, startPoint.time);
 
         while(!Q.isEmpty()) {
-            String u = Q.poll();
-
-            List<LinkedList<StationTime>> neighbors = relations.entrySet().stream().filter(x -> x.getKey().time >= solution.get(u) && x.getKey().pos == u).map(Map.Entry::getValue).collect(Collectors.toList());
-            for(LinkedList<StationTime> neighborsList:neighbors) {
-                for(StationTime neighbor: neighborsList){
-                    if(visited.containsKey(neighbor.pos)){
-                        if(!visited.get(neighbor.pos) && neighbor.time < solution.get(neighbor.pos)) {
-                            solution.put(neighbor.pos, neighbor.time);
-                        }
+            StationTime u = Q.poll();
+            if(solution.containsKey(u.station) && u.time >= solution.get(u.station)) {
+                for(StationTime neighbor:relations.get(u)) {
+                    if(solution.containsKey(neighbor.station)){
+                        if(neighbor.time < solution.get(neighbor.station))
+                            solution.put(neighbor.station, neighbor.time);
                     }
-                    else{
-                        visited.put(neighbor.pos, false);
-                        solution.put(neighbor.pos, neighbor.time);
-                        Q.add(neighbor.pos);
-                    }
+                    else
+                        solution.put(neighbor.station, neighbor.time);
                 }
             }
         }
 
         return solution;
-        //END STRIP
+        // END STRIP
+    }
+
+    public static class StationTime implements Comparable<StationTime> {
+
+        public final String station; 
+        public final int time;  
+    
+        public StationTime(String station, int time) {
+            this.station = station;
+            this.time = time;
+        }
+    
+        @Override
+        public int hashCode() {
+            return station.hashCode() ^ Integer.hashCode(~time);
+        }
+    
+        @Override
+        public boolean equals(Object obj) {
+            if(obj instanceof StationTime)
+                return ((StationTime) obj).station.equals(station) && ((StationTime) obj).time == time;
+            return false;
+        }
+    
+        @Override
+        public int compareTo(StationTime o) {
+            int out = time - o.time;
+            if(out == 0)
+                return station.compareTo(o.station);
+            return out;
+        }
     }
 }
