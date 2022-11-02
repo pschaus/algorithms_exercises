787b79233c2e01a6f2f3668db6849e2e27bf53de
Pierre Schaus <pschaus@gmail.com>
2022-11-02 09:16:32 +0100

    higher, removed iterator (redundant with other question)

--- src/main/java/searching/BinarySearchTreeHigher.java
+++ src/main/java/searching/BinarySearchTreeHigher.java
@@ -5,15 +5,14 @@ import java.util.NoSuchElementException;
 import java.util.Stack;
 
 /**
- * With the given partial implementation of a Binary Search Tree, we ask you to implement
- * the following methods :
+ * With the given partial implementation of a Binary Search Tree,
+ * we ask you to implement the following methods :
  * - minKey() which return the smallest key in the BST, or null if it is empty
- * - higherKey(Key key) which Returns the least key strictly greater than the given key, 
+ * - higherKey(Key key) which returns the least key strictly greater than the given key,
  *   or null if there is no such key
- * 
- * You also need to implement an iterator for the BST
+ *
  */
-public class BST<Key extends Comparable<Key>, Value> implements Iterable<Key> {
+public class BinarySearchTreeHigher<Key extends Comparable<Key>, Value> {
 
     private Node root;             // root of BST
 
@@ -37,7 +36,7 @@ public class BST<Key extends Comparable<Key>, Value> implements Iterable<Key> {
     /**
      * Initializes an empty symbol table.
      */
-    public BST() {}
+    public BinarySearchTreeHigher() {}
 
     /**
      * Returns true if this symbol table is empty.
@@ -89,6 +88,7 @@ public class BST<Key extends Comparable<Key>, Value> implements Iterable<Key> {
     public void put(Key key, Value val) {
         root = put(root, key, val);
     }
+
     private Node put(Node x, Key key, Value val) {
         if (x == null) return new Node(key, val, 1);
         int cmp = key.compareTo(x.key);
@@ -101,37 +101,34 @@ public class BST<Key extends Comparable<Key>, Value> implements Iterable<Key> {
 
 
     /**
-     * TODO 
-     * The expected complexity is O(h) where h is the height of the BST
-     * 
      * Returns the minimum key
-
+     * The expected complexity is O(h) where h is the height of the BST
+     *
      * @return the minimum key, null if the tree is empty
      */
     public Key minKey() {
         // STUDENT return null;
-
         // BEGIN STRIP
         return minKey(root);
         // END STRIP
     }
+
+
     // BEGIN STRIP
     private Key minKey(Node x) {
-        if (x == null) return null;
-        if (x.left == null)
+        if (x == null) {
+            return null;
+        } else if (x.left == null) {
             return x.key;
-        else 
+        } else {
             return minKey(x.left);
+        }
     }
     // END STRIP
 
-
-
     /**
-     * TODO
+     * Returns the smallest key strictly greater than the given key, or null if there is no such key.
      * The expected complexity is O(h) where h is the height of the BST
-     * 
-     * Returns the least key strictly greater than the given key, or null if there is no such key.
      *
      * @param key - the key
      * @return the least key greater than key, or null if there is no such key
@@ -139,101 +136,22 @@ public class BST<Key extends Comparable<Key>, Value> implements Iterable<Key> {
     public Key higherKey(Key key) {
         // STUDENT return null;
         // BEGIN STRIP
-            return higherKey(root, key);
+        return higherKey(root, key);
         // END STRIP
     }
 
     private Key higherKey(Node x, Key key) {
         // STUDENT return null;
         // BEGIN STRIP
-        if (x ==  null) return null;
-
+        if (x == null) return null;
         int cmp = key.compareTo(x.key);
+        // key >= x.key
         if (cmp >= 0) return higherKey(x.right, key);
-
+        // key < x.key
         Key left = higherKey(x.left, key);
         if (left == null) return x.key;
         else return left;
         // END STRIP
     }
 
-
-    /**
-     * TODO
-     * 
-     * Returns an iterator that iterates through the keys in Incresing order
-     * (In-Order transversal).
-     * @return an iterator that iterates through the items in FIFO order.
-     */
-    @Override
-    public Iterator<Key> iterator() {
-        return new BSTIterator();
-    }
-
-    // the iterator doesn't implement remove() optional method
-
-    private class BSTIterator implements Iterator<Key> {
-
-        // TODO feel free to add some state variables
-        // BEGIN STRIP
-        private Stack<Node> stack;
-        private Node current;
-        private int size;
-        // END STRIP
-
-        /*
-         * TODO
-         * 
-         * The expected complexity is O(h) where h is the height of the BST
-         */
-        public BSTIterator() {
-            // BEGIN STRIP
-            stack = new Stack<>();
-            current = root;
-            while (current != null) {
-                stack.push(current);
-                current = current.left;
-            }
-            if (root != null) {
-                size = root.getSize();
-            }
-            // END STRIP
-        }
-
-        /**
-         * TODO
-         * 
-         * The expected complexity is O(1)
-         */
-        public boolean hasNext() {
-            // STUDENT return null;
-            // BEGIN STRIP
-            return !this.stack.isEmpty();
-            // END STRIP
-        }
-
-
-        /*
-         * TODO
-         * 
-         * The expected complexity is O(h) where h is the height of the BST
-         */
-        public Key next() {
-            // STUDENT return null;
-            // BEGIN STRIP
-            if (!hasNext()) throw new NoSuchElementException();
-            Node node = stack.pop();
-            Key key = node.key;
-            if (node.right != null) {
-                node = node.right;
-                while (node != null) {
-                    stack.push(node);
-                    node = node.left;
-                }
-            }
-            return key;
-            // END STRIP
-        }
-
-    }
 }
--- src/test/java/searching/BinarySearchTreeHigherTest.java
+++ src/test/java/searching/BinarySearchTreeHigherTest.java
@@ -5,8 +5,6 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 import com.github.guillaumederval.javagrading.Grade;
-import com.github.guillaumederval.javagrading.GradeFeedback;
-import com.github.guillaumederval.javagrading.GradingRunnerWithParametersFactory;
 import org.junit.runners.Parameterized;
 import org.junit.experimental.runners.Enclosed;
 import org.junit.runner.RunWith;
@@ -16,12 +14,12 @@ import java.io.IOException;
 
 
 @RunWith(Enclosed.class)
-public class BSTTest {
+public class BinarySearchTreeHigherTest {
     public static class TestNotParameterized {
-        public BST<Integer, Integer> getExample() {
+        public BinarySearchTreeHigher<Integer, Integer> getExample() {
 
-            BST<Integer, Integer> bst = new BST<Integer, Integer>();
-    
+            BinarySearchTreeHigher<Integer, Integer> bst = new BinarySearchTreeHigher<Integer, Integer>();
+
             bst.put(12,12);
             bst.put(8,8);
             bst.put(3,3);
@@ -31,61 +29,42 @@ public class BSTTest {
             bst.put(14,14);
             bst.put(15,15);
             bst.put(20,20);
-    
+
             return bst;
         }
-    
-    
+
+
         @Test
         @Grade(value=5)
         public void testMinKeyExample() {
-            BST<Integer, Integer> bst = getExample();
+            BinarySearchTreeHigher<Integer, Integer> bst = getExample();
             assertEquals(Integer.valueOf(3), bst.minKey());
-    
+
         }
-    
+
         @Test
         @Grade(value=10)
         public void testHigherExample() {
-            BST<Integer, Integer> bst = getExample();
+            BinarySearchTreeHigher<Integer, Integer> bst = getExample();
             assertEquals(Integer.valueOf(3), bst.higherKey(0));
             assertEquals(Integer.valueOf(9), bst.higherKey(8));
             assertEquals(Integer.valueOf(12), bst.higherKey(11));
             assertEquals(Integer.valueOf(18), bst.higherKey(16));
             assertEquals(null, bst.higherKey(20));
-    
+
         }
-    
-    
-        @Test
-        @Grade(value=5)
-        public void testIteratorExample() {
-            BST<Integer, Integer> bst = getExample();
-            int[] values = new int[]{3, 8, 9, 11, 12, 14, 15, 18, 20};
-    
-            Iterator<Integer> ite = bst.iterator();
-    
-            int i = 0;
-            while (ite.hasNext()) {
-                int v = ite.next();
-                assertEquals(values[i],v);
-                i++;
-            }
-            assertFalse(ite.hasNext());
-            assertEquals(values.length,i);
-    
-        }
+
     }
 
     @RunWith(Parameterized.class)
-    public  static class BSTTestComplexity {
+    public  static class BinarySearchTreeHigherTestComplexity {
 
 
-        private BST<Integer,Integer> student;
+        private BinarySearchTreeHigher<Integer,Integer> student;
         private TreeSet<Integer> correct;
 
 
-        public BSTTestComplexity(BST<Integer,Integer> student, TreeSet<Integer> correct) {
+        public BinarySearchTreeHigherTestComplexity(BinarySearchTreeHigher<Integer,Integer> student, TreeSet<Integer> correct) {
             this.student = student;
             this.correct = correct;
         }
@@ -111,25 +90,6 @@ public class BSTTest {
         }
 
 
-        @Test(timeout=1000)
-        @Grade(value=1)
-        public void testIterateAll() {
-            Iterator<Integer> aIter = student.iterator();
-            Iterator<Integer> bIter = correct.iterator();
-            while (bIter.hasNext()) {
-                assertEquals(bIter.next(),aIter.next());
-            }
-            assertFalse(aIter.hasNext());
-        }
-
-        @Test(timeout=100)
-        @Grade(value=1)
-        public void testIteratorCreation() {
-            Iterator<Integer> aIter = student.iterator();
-            assertTrue(aIter.hasNext());
-            assertEquals(correct.first(),aIter.next());
-        }
-
 
 
 
@@ -138,7 +98,7 @@ public class BSTTest {
             Random r = new Random();
             LinkedList tests = new LinkedList<>();
             for (int i = 0; i < 5; i++) {
-                BST<Integer,Integer> a = new BST<>();
+                BinarySearchTreeHigher<Integer,Integer> a = new BinarySearchTreeHigher<>();
                 Set<Integer> b = new TreeSet<>();
                 for (int k = 0; k < 100000; k++) {
                     int v = r.nextInt();
@@ -153,13 +113,13 @@ public class BSTTest {
     }
 
     @RunWith(Parameterized.class)
-    public static class BSTTestRandom {
+    public static class BinarySearchTreeHigherTestRandom {
 
-        private BST<Integer,Integer> student;
+        private BinarySearchTreeHigher<Integer,Integer> student;
         private TreeSet<Integer> correct;
 
 
-        public BSTTestRandom(BST<Integer,Integer> student, TreeSet<Integer> correct) {
+        public BinarySearchTreeHigherTestRandom(BinarySearchTreeHigher<Integer,Integer> student, TreeSet<Integer> correct) {
             this.student = student;
             this.correct = correct;
         }
@@ -184,19 +144,6 @@ public class BSTTest {
             }
         }
 
-        @Test
-        @Grade(value=0.3)
-        public void testIterator() {
-            Iterator<Integer> aIter = student.iterator();
-            Iterator<Integer> bIter = correct.iterator();
-            assertEquals(correct.size(),student.size());
-            while (bIter.hasNext()) {
-                assertTrue(aIter.hasNext());
-                assertEquals(bIter.next(),aIter.next());
-            }
-            assertFalse(bIter.hasNext());
-            assertFalse(aIter.hasNext());
-        }
 
 
         @Parameterized.Parameters
@@ -205,7 +152,7 @@ public class BSTTest {
             int min=1, max=26;
             LinkedList tests = new LinkedList<>();
             for (int i = 0; i < 50; i++) {
-                BST<Integer,Integer> a = new BST<>();
+                BinarySearchTreeHigher<Integer,Integer> a = new BinarySearchTreeHigher<>();
                 Set<Integer> b = new TreeSet<>();
                 for (int k = 0; k < 15; k++) {
                     int v = r.nextInt((max - min) + 1) + min;
