1e241c19b65f53af1e423cb901fe0505f6ba8bc2
Pierre Schaus <pschaus@gmail.com>
2022-12-05 15:34:14 +0100

    bug fix LinkedHeap solution

--- src/test/java/sorting/MinPQLinkedTest.java
+++ src/test/java/sorting/MinPQLinkedTest.java
@@ -26,6 +26,75 @@ public class MinPQLinkedTest {
     }
 
 
+    public static boolean isEssentiallyComplete(MinPQLinked<Integer>.Node n) {
+        boolean [] visited = new boolean[n.size+1];
+        collect(n,visited,1);
+        for (int i = 1; i <= n.size ; i++) {
+            if (!visited[i]) return false;
+        }
+        return true;
+    }
+
+    private static void collect(MinPQLinked<Integer>.Node n, boolean [] visited, int index) {
+       visited[index] = true;
+       if (n.left  != null) collect(n.left, visited, index*2);
+       if (n.right != null) collect(n.right, visited, index*2+1);
+    }
+
+
+
+
+    @Test
+    @Grade(value=1, cpuTimeout=1000)
+    public void testDeleteMin0() {
+        MinPQLinked<Integer> pq = new MinPQLinked(Comparator.naturalOrder());
+        pq.insert(-20);
+        pq.insert(-15);
+        pq.insert(-18);
+        pq.insert(-14);
+        pq.insert(-11);
+        pq.insert(-10);
+        pq.insert(-9);
+        pq.insert(-8);
+        pq.insert(-13);
+        pq.insert(-5);
+        pq.insert(-2);
+        pq.insert(-5);
+        pq.insert(-8);
+
+        assertEquals(13, (int) pq.size());
+        assertTrue(isMinHeapOrdered(pq.root));
+        assertTrue(isEssentiallyComplete(pq.root));
+
+        assertEquals(-20, (int) pq.delMin());
+
+        assertEquals(-18, (int) pq.delMin());
+
+        assertTrue(isMinHeapOrdered(pq.root));
+        assertTrue(isEssentiallyComplete(pq.root));
+
+        assertEquals(11, (int) pq.size());
+
+        assertEquals(-15, (int) pq.delMin());
+        assertEquals(-14, (int) pq.delMin());
+
+        assertEquals(9, (int) pq.size());
+
+        assertEquals(-13, (int) pq.delMin());
+
+        assertEquals(-11, (int) pq.delMin());
+        assertEquals(-10, (int) pq.delMin());
+
+        assertTrue(isMinHeapOrdered(pq.root));
+        assertTrue(isEssentiallyComplete(pq.root));
+
+        assertEquals(-9, (int) pq.delMin());
+        assertEquals(-8, (int) pq.delMin());
+        assertEquals(-8, (int) pq.delMin());
+        assertEquals(-5, (int) pq.delMin());
+        assertEquals(-5, (int) pq.delMin());
+        assertEquals(-2, (int) pq.delMin());
+    }
 
     @Test
     @Grade(value=1, cpuTimeout=1000)
@@ -52,6 +121,8 @@ public class MinPQLinkedTest {
         assertEquals(3,(int) pq.delMin());
 
         assertEquals(3,(int) pq.size());
+        assertTrue(isMinHeapOrdered(pq.root));
+        assertTrue(isEssentiallyComplete(pq.root));
 
         assertEquals(4,(int) pq.delMin());
         assertEquals(6,(int) pq.delMin());
@@ -85,10 +156,12 @@ public class MinPQLinkedTest {
         assertEquals(1,(int) pq.delMin());
         assertEquals(2,(int) pq.delMin());
         assertTrue(isMinHeapOrdered(pq.root));
+        assertTrue(isEssentiallyComplete(pq.root));
         assertEquals(3,(int) pq.delMin());
 
         assertEquals(3,(int) pq.size());
         assertTrue(isMinHeapOrdered(pq.root));
+        assertTrue(isEssentiallyComplete(pq.root));
         assertEquals(4,(int) pq.delMin());
         assertEquals(6,(int) pq.delMin());
         assertEquals(7,(int) pq.delMin());
@@ -120,6 +193,7 @@ public class MinPQLinkedTest {
         pq.insert(1);
         pq.insert(2);
         assertTrue(isMinHeapOrdered(pq.root));
+        assertTrue(isEssentiallyComplete(pq.root));
         assertEquals(2,(int) pq.size());
 
         assertEquals(1,(int) pq.delMin());
@@ -137,11 +211,13 @@ public class MinPQLinkedTest {
         pq.insert(5);
         pq.insert(7);
         assertTrue(isMinHeapOrdered(pq.root));
+        assertTrue(isEssentiallyComplete(pq.root));
         assertEquals(3,(int) pq.size());
 
         assertEquals(1,(int) pq.delMin());
         assertEquals(2,(int) pq.size());
         assertTrue(isMinHeapOrdered(pq.root));
+        assertTrue(isEssentiallyComplete(pq.root));
         assertEquals(5,(int) pq.delMin());
         assertEquals(7,(int) pq.delMin());
         assertEquals(0,(int) pq.size());
