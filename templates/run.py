71f9c5c790e627e6c274a5f47601df4da7a280f7
Alexandre Dubray <alexandre.dubray@uclouvain.be>
2023-09-28 09:24:07 +0200

    add output of mvn when compilation errors

--- templates/run.py
+++ templates/run.py
@@ -18,30 +18,36 @@ out = subprocess.Popen(f'mvn -q -am test',
                        stderr=None if fail_if_stderr else subprocess.DEVNULL).stdout.read()
 
 out = out.decode().split("\n")
+try:
+    start = -1
+    end = -1
+    for i, line in enumerate(out):
+        if line == "--- GRADE ---":
+            start = i
+        if line == "--- END GRADE ---":
+            end = i
+    assert start < end
+    assert start != -1
+    assert end != -1
+    out = out[start:end+1]
 
-start = -1
-end = -1
-for i, line in enumerate(out):
-    if line == "--- GRADE ---":
-        start = i
-    if line == "--- END GRADE ---":
-        end = i
-assert start < end
-assert start != -1
-assert end != -1
-out = out[start:end+1]
+    def get_grade(line):
+        match = re.search(r"^TOTAL *([0-9\.]+)/([0-9\.]+)$", line)
+        a = float(match.group(1))
+        b = float(match.group(2))
+        return a/b if b != 0 else 0.0
 
-def get_grade(line):
-    match = re.search(r"^TOTAL *([0-9\.]+)/([0-9\.]+)$", line)
-    a = float(match.group(1))
-    b = float(match.group(2))
-    return a/b if b != 0 else 0.0
+    g = get_grade(out[-3])
 
-g = get_grade(out[-3])
+    g *= 100
+    g = int(round(g))
 
-g *= 100
-g = int(round(g))
-
-feedback.set_global_result("failed" if g != 100 else "success")
-feedback.set_grade(g)
-feedback.set_global_feedback("\n".join(out[1:-3]), append=True)
+    feedback.set_global_result("failed" if g != 100 else "success")
+    feedback.set_grade(g)
+    feedback.set_global_feedback("\n".join(out[1:-3]), append=True)
+except:
+    feedback.set_global_result("failed")
+    feedback.set_grade(0.0)
+    mvn_feedback = rst.get_codeblock("bash", "\n".join(out))
+    feedback.set_global_feedback("An error occured while grading. Here is the output of the command `mvn -q -am test`:", append=True)
+    feedback.set_global_feedback(mvn_feedback, append=True)
