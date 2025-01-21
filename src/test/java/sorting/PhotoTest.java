9975eca68be5678e9d1dc77f42d2f0e51916d4eb
BastienAubecq <bastien.aubecq@student.uclouvain.be>
2025-01-21 18:35:13 +0100

    Add tests for Photo The tests could be passed without sorting teamB and without checking equal height.

--- src/test/java/sorting/PhotoTest.java
+++ src/test/java/sorting/PhotoTest.java
@@ -28,6 +28,10 @@ public class PhotoTest {
         int [] teamB = new int[]{160, 172, 190, 185};
         assertEquals(36, Photo.canTakePictures(teamA, teamB));
         assertEquals(36, Photo.canTakePictures(teamB, teamA));
+        teamA = new int[]{264, 285, 117, 239};
+        teamB = new int[]{286, 121, 240, 292};
+        assertEquals(34, Photo.canTakePictures(teamA, teamB));
+        assertEquals(34, Photo.canTakePictures(teamB, teamA));
     }
     
     @Test
@@ -36,6 +40,10 @@ public class PhotoTest {
     public void testExampleFalse() {
         int [] teamA = new int[]{144, 173, 158, 195};
         int [] teamB = new int[]{152, 169, 165, 189};
+        assertEquals(-1, Photo.canTakePictures(teamA, teamB));
+        assertEquals(-1, Photo.canTakePictures(teamB, teamA));
+        teamA = new int[]{283, 199, 314, 485};
+        teamB = new int[]{523, 296, 314, 264};
         assertEquals(-1, Photo.canTakePictures(teamA, teamB));
         assertEquals(-1, Photo.canTakePictures(teamB, teamA));
     }
