d9476d05aa9cf879b6e4f2a71a6ca0eaac70e9a2
Kaan <145116515+khaixmania@users.noreply.github.com>
2025-09-29 14:37:52 +0200

    Update FList.java
    Rectified version of length()

--- src/main/java/fundamentals/FList.java
+++ src/main/java/fundamentals/FList.java
@@ -56,7 +56,10 @@ public abstract class FList<A> implements Iterable<A> {
         // TODO
         // STUDENT return -1;
         // BEGIN STRIP
-        return -1;
+        if (isEmpty()) return 0;
+        else {
+            return 1 + tail().length();
+        }
         // END STRIP;
     }
 
