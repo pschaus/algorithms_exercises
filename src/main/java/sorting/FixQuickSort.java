a65403a6b1a6e18e35b41a208e9b4729364553a8
pschaus <pschaus@gmail.com>
2025-10-08 09:15:02 +0200

    fix quick sort

---
+++ src/main/java/sorting/FixQuickSort.java
@@ -0,0 +1,99 @@
+package sorting;
+
+
+import java.util.Arrays;
+import java.util.Random;
+
+/**
+ * This is a quick sort implementation for integers.
+ * It works correctly, but unfortunately, has you can see in the test,
+ * it has a very bad worst-case performance when all values are the same.
+ * Since all the values are the same, the pre-shuffle does not help in this case.
+ * What is wrong with the implementation? Can you fix it?
+ * What is the complexity of the current implementation when all values are the same?
+ * What is the complexity of your fixed implementation in this case?
+ */
+public class FixQuickSort {
+
+    /**
+     * Rearranges the array in ascending order, using the natural order.
+     * @param a the array to be sorted
+     */
+    public static void sort(int[] a) {
+        // shuffle array to avoid worst-case
+        shuffleArray(a);
+        sort(a, 0, a.length - 1);
+        assert isSorted(a);
+    }
+
+    // quicksort the subarray from a[lo] to a[hi]
+    private static void sort(int[] a, int lo, int hi) {
+        if (hi <= lo) return;
+        int j = partition(a, lo, hi);
+        sort(a, lo, j-1);
+        sort(a, j+1, hi);
+        assert isSorted(a, lo, hi);
+    }
+
+    // partition the subarray a[lo..hi] so that a[lo..j-1] <= a[j] <= a[j+1..hi]
+    // and return the index j.
+    private static int partition(int[] a, int lo, int hi) {
+        int i = lo;
+        int j = hi + 1;
+        int v = a[lo];
+        while (true) {
+            // find item on lo to swap
+            while (a[++i] <= v) {
+                if (i == hi) break;
+            }
+            // find item on hi to swap
+            while (v <= a[--j]) {
+                if (j == lo) break;      // redundant since a[lo] acts as sentinel
+            }
+            // check if pointers cross
+            if (i >= j) break;
+            exch(a, i, j);
+        }
+        // put partitioning item v at a[j]
+        exch(a, lo, j);
+        // now, a[lo .. j-1] <= a[j] <= a[j+1 .. hi]
+        return j;
+    }
+
+
+
+    // exchange a[i] and a[j]
+    private static void exch(int[] a, int i, int j) {
+        int swap = a[i];
+        a[i] = a[j];
+        a[j] = swap;
+    }
+
+    // Fisher–Yates shuffle
+    public static void shuffleArray(int[] ar) {
+        Random rnd = new Random();
+        for (int i = ar.length - 1; i > 0; i--) {
+            int index = rnd.nextInt(i + 1);
+            // Simple swap
+            int a = ar[index];
+            ar[index] = ar[i];
+            ar[i] = a;
+        }
+    }
+
+
+    /***************************************************************************
+     *  Check if array is sorted - useful for debugging and testing, do not modify
+     ***************************************************************************/
+
+    public static boolean isSorted(int[] a) {
+        return isSorted(a, 0, a.length - 1);
+    }
+
+    private static boolean isSorted(int[] a, int lo, int hi) {
+        for (int i = lo + 1; i <= hi; i++)
+            if (a[i-1] > a[i]) return false;
+        return true;
+    }
+
+}
---
+++ src/test/java/searching/FixQuickSortTest.java
@@ -0,0 +1,62 @@
+package searching;
+
+import org.javagrader.ConditionalOrderingExtension;
+import org.javagrader.Grade;
+import org.javagrader.GradeFeedback;
+import org.javagrader.TestResultStatus;
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.extension.ExtendWith;
+import sorting.FixQuickSort;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+import static org.junit.jupiter.api.Assertions.*;
+// END STRIP
+
+@Grade
+@ExtendWith(ConditionalOrderingExtension.class)
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+public class FixQuickSortTest {
+
+    // BEGIN STRIP
+    Random random = new Random(546543);
+    // END STRIP
+
+
+    @Test
+    @Grade(value = 1, cpuTimeout = 1000, unit = TimeUnit.MILLISECONDS, threadMode = Timeout.ThreadMode.SEPARATE_THREAD)
+    @GradeFeedback(message = "Your implementation does not finish in the allocated time.", on = TestResultStatus.FAIL)
+    @Order(3)
+    public void testLarge() {
+        int n = 100000;
+        int[] array = IntStream.range(0, n).toArray();
+        FixQuickSort.shuffleArray(array);
+        FixQuickSort.sort(array);
+        assertTrue(FixQuickSort.isSorted(array));
+        for (int i = 0; i < n; i++) {
+            assertEquals(i, array[i]);
+        }
+    }
+
+    @Test
+    @Grade(value = 1, cpuTimeout = 1000, unit = TimeUnit.MILLISECONDS, threadMode = Timeout.ThreadMode.SEPARATE_THREAD)
+    @GradeFeedback(message = "Your implementation does not finish in the allocated time.", on = TestResultStatus.FAIL)
+    @Order(3)
+    public void testLargeSameValue() {
+        int n = 100000;
+        int[] array = new int[n]; // all zeros
+        FixQuickSort.shuffleArray(array);
+        FixQuickSort.sort(array);
+        assertTrue(FixQuickSort.isSorted(array));
+    }
+
+
+
+
+
+}
